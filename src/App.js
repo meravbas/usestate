485dfda737f823a40b747dccb23cab852bf26573
Merav Bas <meravbas71@gmail.com>
2022-11-22 18:37:54 +0200

    Lesson 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Counter from './Counter';
+import Stylesheet from './Stylesheet';
 function App() {
   return (
     <div className="App">
       Usestate
       <h1> another line</h1>
       <Counter/>
+      <Stylesheet/>
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 function Counter() {
     const [count, setCount] = useState(1)
@@ -9,11 +10,23 @@ function Counter() {
         )
         console.log(count)
     }
+    function Reset () {
+        setCount(
+            function(oldCount){
+                return 1
+            }
+        )
+        console.log(count)
+    }
     return (
       <div> 
         <h1> Counter</h1>
         <p>Counter is at {count} </p>
+        
         <button onClick={inc}> Click to add 1 to Counter</button>
+        <p> 
+        <button onClick={Reset}> Click to reset the Counter</button>
+        </p>
       </div>
     );
   }
