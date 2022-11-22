ec64ee0b912854d1039d8a9035fd3c157022de37
Merav Bas <meravbas71@gmail.com>
2022-11-22 17:48:04 +0200

    Added useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
-
+import Counter from './Counter';
 function App() {
   return (
     <div className="App">
       Usestate
       <h1> another line</h1>
+      <Counter/>
     </div>
   );
 }
